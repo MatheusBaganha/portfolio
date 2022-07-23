25f11e6a68a840b1356ceacdbcbd2a2063088b41
MatheusBaganha <matheusu.baganha@gmail.com>
2022-07-23 15:21:12 -0300

    efeito escrever e codigo comentado

--- js/modules/AnimaScroll.js
+++ js/modules/AnimaScroll.js
@@ -1,26 +1,22 @@
+import debounce from './debounce.js';
+
 export default class AnimaScroll {
   constructor(animateContents, classAnimate) {
+    // animateContents são todos os containers que devem ser animados
     this.animateContents = document.querySelectorAll(animateContents);
+    // classAnimate será o nome da classe css que irá fazer a animação
     this.classAnimate = classAnimate;
 
     this.animateOnScroll = this.animateOnScroll.bind(this);
 
-    this.animateOnScroll();
+    //  Animação e eventos adicionados já inicialmente para que não fique com tela preta
     this.addEvents();
-  }
-
-  debounce(callback, delay) {
-    let timer;
-    return (...args) => {
-      if (timer) clearTimeout(timer);
-      timer = setTimeout(() => {
-        callback(...args);
-        timer = null;
-      }, delay);
-    };
+    this.animateOnScroll();
   }
 
   animateOnScroll() {
+    // Espera até chegar a quase 1/4 da tela em relação ao container para ativar
+    // e dar a classe de animação
     const windowTop = window.scrollY + ((window.innerHeight * 3) / 4);
     this.animateContents.forEach((content) => {
       if (windowTop > (content.offsetTop)) {
@@ -30,8 +26,9 @@ export default class AnimaScroll {
   }
 
   addEvents() {
+    // Se existir conteudo para ser animado, adicione o evento
     if (this.animateContents.length) {
-      window.addEventListener('scroll', this.debounce(this.animateOnScroll, 200));
+      window.addEventListener('scroll', debounce(this.animateOnScroll, 200));
     }
   }
 }
--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ import MenuMobile from './modules/MenuMobile.js';
 import Slider from './modules/slider.js';
 import ScrollSuave from './modules/ScrollSuave.js';
 import AnimaScroll from './modules/AnimaScroll.js';
+import EscreverEfeito from './modules/EscreverEfeito.js';
 
 const Menu = new MenuMobile('[data-nav]', '[data-btn]', 'aberto', '[data-header]');
 Menu.init();
@@ -11,3 +12,5 @@ const slider = new Slider(1, '[data-slides] li', '[data-navSlide] .label', '[dat
 const scrollSuave = new ScrollSuave('[data-links] a[href^="#"]');
 
 const animaScroll = new AnimaScroll('[data-anime]', 'animar');
+
+const escreverEfeito = new EscreverEfeito('[data-escrever]');
