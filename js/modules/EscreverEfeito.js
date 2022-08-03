5b02244b2e58224875ca77534d6b876072e3fc30
MatheusBaganha <matheusu.baganha@gmail.com>
2022-08-03 18:34:19 -0300

    otimização

--- js/modules/EscreverEfeito.js
+++ js/modules/EscreverEfeito.js
@@ -1,1 +1,29 @@
-export default class EscreverEfeito{constructor(e){this.escreverTextos=document.querySelectorAll(e),this.ativaEscrever=this.ativaEscrever.bind(this),this.ativaEscrever(this.escreverTexto)}escrever(e){const r=e.innerText.split("");e.innerHTML="",r.forEach(((r,t)=>{setTimeout((()=>e.innerHTML+=r),18*t)}))}ativaEscrever(){this.escreverTextos.forEach((e=>{this.escrever(e)}))}}
+export default class EscreverEfeito {
+  constructor(escreverTexto) {
+    this.escreverTextos = document.querySelectorAll(escreverTexto);
+    //  escreverTextos é todos os textos que será escrito na tela
+
+    // Mudança de contexto do this de ativaEscrever e ativação inicial no texto que deve ser escrito
+    this.ativaEscrever = this.ativaEscrever.bind(this);
+    this.ativaEscrever(this.escreverTexto);
+  }
+
+  escrever(elemento) {
+    // Responsável pela animação de cada letra sendo escrita uma após a outra
+    const textoArray = elemento.innerText.split('');
+    // textoArray possui todas as letras do texto a ser escrevido
+
+    // Limpa o texto do elemento para não mostrar nada na tela inicialmente
+    elemento.innerHTML = '';
+
+    // Coloca letra por letra na tela
+    textoArray.forEach((letra, i) => { setTimeout(() => elemento.innerHTML += letra, 18 * i); });
+  }
+
+  ativaEscrever() {
+    // Ativa o escrever para cada texto do escreverTextos
+    this.escreverTextos.forEach((texto) => {
+      this.escrever(texto);
+    });
+  }
+}
--- js/modules/ScrollSuave.js
+++ js/modules/ScrollSuave.js
@@ -1,1 +1,32 @@
-export default class ScrollSuave{constructor(t){this.linksInternos=document.querySelectorAll(t),this.addEvents()}handleScroll(t){t.preventDefault();const e=t.target.getAttribute("href"),o=document.querySelector(e).offsetTop;window.scroll({top:o+48,behavior:"smooth"})}addEvents(){this.linksInternos.forEach((t=>{t.addEventListener("click",this.handleScroll)}))}}
+export default class ScrollSuave {
+  constructor(linksInternos) {
+    // Seleciona todos os links internos da pagina
+    this.linksInternos = document.querySelectorAll(linksInternos);
+
+    // Ativa os eventos já inicialmente
+    this.addEvents();
+  }
+
+  handleScroll(event) {
+    //  Previne o comportamento padrão do link, pega o nome do # de href do link
+    event.preventDefault();
+    const link = event.target;
+    const id = link.getAttribute('href');
+
+    //  Pega o tamanho de cada section em relação ao topo baseada no id do link
+    const sectionTop = document.querySelector(id).offsetTop;
+
+    window.scroll({
+      // +48 para alinhar de forma mais centralizada o conteudo
+      top: sectionTop + 48,
+      behavior: 'smooth',
+    });
+  }
+
+  addEvents() {
+    // Adiciona eventos de click para cada link interno
+    this.linksInternos.forEach((item) => {
+      item.addEventListener('click', this.handleScroll);
+    });
+  }
+}
